32c3fb995c1363caa9899e750d109ff369ff99d2
Aarohan Kaji Shikhrakar <aarohan.ks@gmail.com>
2026-04-15 11:35:15 +0545

    Updated functionality using array methods

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -10,9 +10,9 @@ export const TodoProvider = ({ children }) => {
   const [editId, seteditId] = useState(null);
   const [filter, setFilter] = useState("All");
 
-  useEffect(()=>{
-    console.log(todos)
-  },[todos])
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,17 +21,10 @@ export const TodoProvider = ({ children }) => {
 
     if (editId !== null) {
       settodos((curerrentodos) => {
-        const newEdit = [];
-        let length = 0;
-        for (let i = 0; i < curerrentodos.length; i++) {
-          if (curerrentodos[i].id === editId) {
-            newEdit[length] = { ...curerrentodos[i], task: newItem };
-          } else {
-            newEdit[length] = curerrentodos[i];
-          }
-          length++;
-        }
-        return newEdit;
+        const newTodos = curerrentodos.map((todo) =>
+          todo.id === editId ? { ...todo, task: newItem } : todo,
+        );
+        return newTodos;
       });
       seteditId(null);
     } else {
@@ -47,96 +40,86 @@ export const TodoProvider = ({ children }) => {
   };
 
   const editTodo = (id) => {
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i].id === id) {
-        setNewItem(todos[i].task);
-        seteditId(todos[i].id);
-        break;
-      }
-    }
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo === undefined) return;
+
+    setNewItem(todo.task);
+    seteditId(todo.id);
   };
-  // use splice mthod in this 
+
+  // use splice mthod in this
+
   const deleteTodo = (id) => {
     settodos((curerrentodos) => {
-      const newtodos = [];
-      let length = 0;
-      for (let i = 0; i < curerrentodos.length; i++) {
-        if (curerrentodos[i].id !== id) {
-          newtodos[length] = curerrentodos[i];
-          length++;
-        }
+      const index = curerrentodos.findIndex((todo) => todo.id === id);
+      const newTodos = [...curerrentodos];
+      if (index === -1) {
+        return curerrentodos;
+      } else {
+        newTodos.splice(index, 1);
+        return newTodos;
       }
-      return newtodos;
     });
-
   };
 
   const aarowUp = (index) => {
     settodos((curerrentodos) => {
-      const newtodos = [...curerrentodos];
-      if (index === 0) return curerrentodos;
-
-      const temp = newtodos[index];
-      newtodos[index] = newtodos[index - 1];
-      newtodos[index - 1] = temp;
-
-      return newtodos;
+      const newTodos = [...curerrentodos];
+      if (index === 0) {
+        return newTodos;
+      } else {
+        const [item] = newTodos.splice(index, 1); // probkem occurred storing obejct inside if i did item =
+        newTodos.splice(index - 1, 0, item);
+      }
+      return newTodos;
     });
   };
 
   const aarowDown = (index) => {
     settodos((curerrentodos) => {
       const newTodos = [...curerrentodos];
-      if (index === newTodos.length - 1) return curerrentodos;
-      const temp = newTodos[index];
-      newTodos[index] = newTodos[index + 1];
-      newTodos[index + 1] = temp;
-
+      if (index === curerrentodos.length - 1) {
+        return newTodos;
+      } else {
+        const [item] = newTodos.splice(index, 1);
+        newTodos.splice(index + 1, 0, item);
+      }
       return newTodos;
     });
   };
 
+  // not working
   const toggleCheck = (id) => {
     settodos((curerrentodos) => {
-      const updatedTodo = [];
-      let length = 0;
-      for (let i = 0; i < curerrentodos.length; i++) {
-        if (curerrentodos[i].id === id) {
-          updatedTodo[length] = {
-            ...curerrentodos[i],
-            completed: !curerrentodos[i].completed,
-          };
-        } else {
-          updatedTodo[length] = { ...curerrentodos[i] };
-        }
-        length++;
-      }
-      return updatedTodo;
+      return curerrentodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
     });
   };
 
   // this still not worked
 
- const getFilteredTodos = () => {
-  if (filter === "All") return todos;
+  const getFilteredTodos = () => {
+    if (filter === "All") return todos;
 
-  if (filter === "Completed") {
-    return todos.filter((todo) => todo.completed === true);
-  }
-
-  if (filter === "Active") {
-    return todos.filter((todo) => todo.completed === false);
-  }
-};
+    if (filter === "Completed") {
+      return todos.filter((todo) => todo.completed === true);
+    }
+    if (filter === "Active") {
+      return todos.filter((todo) => todo.completed === false);
+    }
+    return todos;
+  };
 
   const clearButton = () => {
-    settodos([]); 
-  }
+    settodos([]);
+  };
   //functionName(()=>{})
 
   // useEffect ( ()=>{
   //   console.log("Button Clicked"),[filter]
   // })
+
   return (
     <>
       <TodoContext.Provider
@@ -154,7 +137,7 @@ export const TodoProvider = ({ children }) => {
           filter,
           getFilteredTodos,
           toggleCheck,
-          clearButton
+          clearButton,
         }}
       >
         {children}
@@ -178,3 +161,91 @@ export default TodoContext;
 //           t.id === editId ? { ...t, task: newItem } : t,
 //         ),
 //       );
+
+// const deleteTodo = (id) => {
+//     settodos((curerrentodos) => {
+//       const newtodos = [];
+//       let length = 0;
+//       for (let i = 0; i < curerrentodos.length; i++) {
+//         if (curerrentodos[i].id !== id) {
+//           newtodos[length] = curerrentodos[i];
+//           length++;
+//         }
+//       }
+//       return newtodos;
+//     });
+
+//   };
+
+// const aarowUp = (index) => {
+//   settodos((curerrentodos) => {
+//     const newtodos = [...curerrentodos];
+//     if (index === 0) return curerrentodos;
+
+//     const temp = newtodos[index];
+//     newtodos[index] = newtodos[index - 1];
+//     newtodos[index - 1] = temp;
+
+//     return newtodos;
+//   });
+// };
+
+//   const arrowDown = (index) => {
+//   settodos((currentTodos) => {
+//     const newtodos = [...currentTodos];
+
+//     if (index === newtodos.length - 1) return currentTodos;
+
+//     const temp = newtodos[index];
+//     newtodos[index] = newtodos[index + 1];
+//     newtodos[index + 1] = temp;
+
+//     return newtodos;
+//   });
+// };
+
+// const editTodo = (id) => {
+//   for (let i = 0; i < todos.length; i++) {
+//     if (todos[i].id === id) {
+//       setNewItem(todos[i].task);
+//       seteditId(todos[i].id);
+//       break;
+//     }
+//   }
+// };
+
+//  if (editId !== null) {
+//     settodos((curerrentodos) => {
+//       const newEdit = [];
+//       let length = 0;
+//       for (let i = 0; i < curerrentodos.length; i++) {
+//         if (curerrentodos[i].id === editId) {
+//           newEdit[length] = { ...curerrentodos[i], task: newItem };
+//         } else {
+//           newEdit[length] = curerrentodos[i];
+//         }
+//         length++;
+//       }
+//       return newEdit;
+//     });
+//     seteditId(null);
+//   } else {
+
+//   const toggleCheck = (id) => {
+//   settodos((curerrentodos) => {
+//     const updatedTodo = [];
+//     let length = 0;
+//     for (let i = 0; i < curerrentodos.length; i++) {
+//       if (curerrentodos[i].id === id) {
+//         updatedTodo[length] = {
+//           ...curerrentodos[i],
+//           completed: !curerrentodos[i].completed,
+//         };
+//       } else {
+//         updatedTodo[length] = { ...curerrentodos[i] };
+//       }
+//       length++;
+//     }
+//     return updatedTodo;
+//   });
+// };
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -2,20 +2,16 @@ import { useContext } from "react";
 import React from "react";
 import { TodoContext } from "./TodoContext";
 import { Button } from "./components/Button";
+import Input from "./components/Input";
 
 const TodoForm = () => {
-  const { handleSubmit, setNewItem, newItem } = useContext(TodoContext);
+  const { handleSubmit } = useContext(TodoContext);
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <div className="heading">My Tasks</div>
       <p className="sec-heading">Manage Your daily tasks.</p>
       <div className="sec-container">
-        <input
-          className="input"
-          placeholder="Add your Todos"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-        />
+        <Input></Input>
         <Button type={"submit"} > + Add Tasks </Button>
       </div>
       
